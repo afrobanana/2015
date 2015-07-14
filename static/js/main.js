d117968546f4f1c95ccdbc3b7e7a307e2c3d8d9d
Nicolas R <rudasn@gmail.com>
2015-07-14 18:46:25 +0300

    Updates

--- static/js/main.js
+++ static/js/main.js
@@ -4,5 +4,30 @@ $(function() {
     offset: 40
   });
   $('.menu').sticky();
+
+  $('.menu').smoothScroll({
+    targets: 'a[href^=#]:not([href=#])',
+    duration: 800
+  });
+  $('.block--timetable').smoothScroll({
+    targets: 'a[href^=#]:not([href=#])',
+    duration: 800,
+    offset: -200
+  });
 });
+
+$.fn.smoothScroll = function(options) {
+  options.offset = options.offset || 0;
+  options.duration = options.duration || 500;
+  return this.on('click', options.targets, function(e) {
+    var hash = $(e.target).closest(options.targets).eq(0).attr('href') || '',
+        target = hash ? $(hash) : [];
+    target = target.length ? target : $('[name=' + hash.slice(1) +']');
+    if (hash && target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top + options.offset
+        }, options.duration);
+    }
+  });
+};
 
