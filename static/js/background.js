b065039dd88ef2bfa5a1ced0c13be053a1683e7e
Nicolas R <rudasn@gmail.com>
2015-07-23 14:10:12 +0300

    Updates

--- static/js/background.js
+++ static/js/background.js
@@ -1,3 +1,4 @@
+var bgs = [];
 // http://codepen.io/AlienPiglet/pen/hvekG
 function background(id) {
   var c = document.getElementById(id);
@@ -5,7 +6,9 @@ function background(id) {
   var xMax = c.width = window.innerWidth;
   var yMax = c.height = window.innerHeight;
   var hmTimes = Math.round(xMax + yMax);
-
+  if (bgs.indexOf(id) === -1) {
+    bgs.push(id);
+  }
   for(var i=0; i<=hmTimes; i++) {
     var randomX = Math.floor((Math.random()*xMax)+1);
     var randomY = Math.floor((Math.random()*yMax)+1);
@@ -20,41 +23,9 @@ function background(id) {
     ctx.fillStyle = "hsla("+randomHue+", 30%, 80%, ."+randomOpacityOne+randomOpacityTwo+")";
     ctx.fillRect(randomX, randomY, randomSize, randomSize);
   }
-  window.addEventListener('resize', throttle(function() {
-    requestAnimationFrame(function() {
-      background(id);
-    });
-  }, 1000, {leading: false}), false, false);
 }
-// http://underscorejs.org/underscore.js
-function throttle(func, wait, options) {
-  var context, args, result;
-  var timeout = null;
-  var previous = 0;
-  if (!options) options = {};
-  var later = function() {
-    previous = options.leading === false ? 0 : Date.now();
-    timeout = null;
-    result = func.apply(context, args);
-    if (!timeout) context = args = null;
-  };
-  return function() {
-    var now = Date.now();
-    if (!previous && options.leading === false) previous = now;
-    var remaining = wait - (now - previous);
-    context = this;
-    args = arguments;
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
-      }
-      previous = now;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining);
-    }
-    return result;
-  };
-}
+window.addEventListener('resize', function() {
+  requestAnimationFrame(function() {
+    bgs.forEach(background);
+  });
+}, false, false);
